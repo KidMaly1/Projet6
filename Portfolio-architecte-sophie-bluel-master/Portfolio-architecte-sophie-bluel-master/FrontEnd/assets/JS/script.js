b5f3be86cc7de84acb7008df23b0a916b5f3c37b
Andria T Rama <titouslef@gmail.com>
2024-01-16 13:12:48 +0100

    secondcommitProjet-

--- Portfolio-architecte-sophie-bluel-master/Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/JS/script.js
+++ Portfolio-architecte-sophie-bluel-master/Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/JS/script.js
@@ -1,10 +1,48 @@
 
 const reponse = await fetch("http://localhost:5678/api/works");
-const pieces =  await reponse.json();
-console.log(pieces)
+const works =  await reponse.json();
 
-const apiSophie = "http://localhost:5678/api-docs/#/default/get_works"
-fetch(apiSophie)
-.then (response) => {
-    console.log(response)
-}
+
+const gallerycontainer = document.querySelector(".gallery")
+
+for (let i = 0; i < works.length; i++) {
+    
+    const elements = document.createElement("figure")
+    gallerycontainer.appendChild(elements);
+
+    const elementsImage = document.createElement("img");
+
+    elementsImage.src = works[i].imageUrl;
+    
+
+    const elementsTitle = document.createElement("figcaption")
+    elementsTitle.innerText = works[i].title;
+    console.log(elementsTitle)
+    
+    
+    elements.appendChild(elementsImage);
+    elements.appendChild(elementsTitle);
+    
+    
+    }
+
+const btnTri = document.querySelector(".btn-objets")
+btnTri.addEventListener("click", function () {
+    const filtreObjets = works.filter(figure)
+    return figure.name == "Objets";
+    console.log(filtreObjets)
+})
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
