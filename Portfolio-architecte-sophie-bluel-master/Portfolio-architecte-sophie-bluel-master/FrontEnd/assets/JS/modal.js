e4a681430912ad3de42f81445d52534c7a75432d
Andria T Rama <titouslef@gmail.com>
2024-02-23 16:31:41 +0100

    Projet6

--- Portfolio-architecte-sophie-bluel-master/Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/JS/modal.js
+++ Portfolio-architecte-sophie-bluel-master/Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/JS/modal.js
@@ -20,20 +20,51 @@ const modalContainer = document.querySelector(".modalGallery")
 
 
 function genererImagesModale(works) {
+    modalContainer.innerHTML = '';
     for (let i = 0; i < works.length; i++) {
         
         const modalImage = document.createElement("img")
         const deleteButton = document.createElement("i")
+        const modalElements = document.createElement("figure")
         
         deleteButton.classList.add("fa-solid", "fa-trash-can");
         
         modalImage.src = allworks[i].imageUrl;
-        modalContainer.appendChild(modalImage);
-        modalContainer.appendChild(deleteButton);
+        modalElements.appendChild(modalImage);
+        modalElements.appendChild(deleteButton);
+        modalContainer.appendChild(modalElements);
 
-        const deleteWork = document.getElementById("trashButton")
-        deleteWork.addEventListener("click", () => {
-            modalContainer.classList.remove(modalImage);
+	    deleteButton.setAttribute("data-index", allworks[i].id);
+        
+
+        
+        
+        deleteButton.addEventListener("click", function() {
+            console.log(this)
+            console.log(this.getAttribute("data-index"))
+            const worksId = this.getAttribute("data-index");
+
+            fetch("http://localhost:5678/api/works/"+worksId, {
+        method: "DELETE",
+        headers: {"Content-Type": "application/json", 
+                "Authorization":"Bearer "+localStorage.getItem("token")}
+        
+    }).then(function (response) {
+        console.log(response)
+        fetch("http://localhost:5678/api/works").then(function(response){
+            return response.json();
+            
+        }).then(function(allworks){
+            console.log(allworks)
+            genererImagesModale(allworks);
+            //genererTravaux(allworks);
+        });
+
+        
+
+    })
+    
+
         })
 
         
